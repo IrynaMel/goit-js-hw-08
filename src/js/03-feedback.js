d721a68e2e74cdcfdf97c9b77b055e23b02b525b
IrynaMel <iryna.melnyk270692@gmail.com>
2021-12-18 18:00:25 +0200

    videoplayer

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -30,8 +30,10 @@ function onInputChanges(e){
 function onFormChange(){
     const saveMessage = localStorage.getItem('feedback-form-state');
     const parsellingMasseg =  JSON.parse(saveMessage)
-    if(saveMessage){
+    if(parsellingMasseg.email){
         refs.email.value =  parsellingMasseg.email;
+    }
+    if(parsellingMasseg.message){
         refs.textarea.value = parsellingMasseg.message;
     }
 }
