7c98e32ca6cdce2432041f717f03edfb742e21b8
IrynaMel <iryna.melnyk270692@gmail.com>
2021-12-19 20:29:25 +0200

    update feedback

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,25 +8,43 @@ const refs ={
 
 
 refs.form.addEventListener('submit', onFormSubmit);
-const formData ={
-    email: '',
-    message: ''
-}
+
 
 onFormChange()
+
+const saveMessage = localStorage.getItem('feedback-form-state');
+    const parsellingMasseg =  JSON.parse(saveMessage)
+
+function onFormChange(){
+    
+    if(parsellingMasseg){
+       refs.email.value =  parsellingMasseg.email;
+       refs.textarea.value =  parsellingMasseg.message; 
+       
+    } 
+}
+let formData ={
+    email:"",
+    message:""
+}
 
 function onFormSubmit(e){
     e.preventDefault();
     e.currentTarget.reset()
     localStorage.removeItem('feedback-form-state')
-    console.log(formData);
-}
+    if(parsellingMasseg){
+        console.log(parsellingMasseg)
+    } 
+    else
+     {console.log(formData);}
+    
+    }
 
 
 refs.form.addEventListener("input", throttle(onInputChanges, 500 ));
 
 function onInputChanges(e){
-    formData[e.target.name] = e.target.value;
+     formData[e.target.name] = e.target.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(formData))
 };
 
